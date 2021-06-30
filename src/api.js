7c62df18ce841372c5f7c01a586fbb638a77bd02
Carlos Priego <carlos.priego@ibm.com>
2021-06-29 23:55:18 -0500

    test(api): dummy horizonal bot game response

--- src/api.js
+++ src/api.js
@@ -8,6 +8,29 @@ class API {
       player: 'X',
     };
   }
+
+  botGame() {
+    return {
+      winner: 'O',
+      board: [' ', 'X', ' ', 'O', 'O', 'O', ' ', 'X', 'X'],
+      movements: [
+        { player: 'X', position: 'top-center' },
+        { player: 'O', position: 'mid-left' },
+        { player: 'X', position: 'bottom-center' },
+        { player: 'O', position: 'mid-center' },
+        { player: 'X', position: 'bottom-right' },
+        { player: 'O', position: 'mid-right' },
+      ],
+      asciiMovements: [
+        ' |X| \n-+-+-\n | | \n-+-+-\n | | ',
+        ' |X| \n-+-+-\nO| | \n-+-+-\n | | ',
+        ' |X| \n-+-+-\nO| | \n-+-+-\n |X| ',
+        ' |X| \n-+-+-\nO|O| \n-+-+-\n |X| ',
+        ' |X| \n-+-+-\nO|O| \n-+-+-\n |X|X',
+        ' |X| \n-+-+-\nO|O|O\n-+-+-\n |X|X',
+      ],
+    };
+  }
 }
 
 module.exports = { API };
--- tests/api.test.js
+++ tests/api.test.js
@@ -14,4 +14,29 @@ describe('API contract tests for the tictactoe game', () => {
 
     expect(api.newGame()).toStrictEqual(expectedResponse);
   });
+
+  test('should return full game with horizontal victory on /api/tictactoe/botgame/horizontal', () => {
+    const expectedResponse = {
+      winner: 'O',
+      board: [' ', 'X', ' ', 'O', 'O', 'O', ' ', 'X', 'X'],
+      movements: [
+        { player: 'X', position: 'top-center' },
+        { player: 'O', position: 'mid-left' },
+        { player: 'X', position: 'bottom-center' },
+        { player: 'O', position: 'mid-center' },
+        { player: 'X', position: 'bottom-right' },
+        { player: 'O', position: 'mid-right' },
+      ],
+      asciiMovements: [
+        ' |X| \n-+-+-\n | | \n-+-+-\n | | ',
+        ' |X| \n-+-+-\nO| | \n-+-+-\n | | ',
+        ' |X| \n-+-+-\nO| | \n-+-+-\n |X| ',
+        ' |X| \n-+-+-\nO|O| \n-+-+-\n |X| ',
+        ' |X| \n-+-+-\nO|O| \n-+-+-\n |X|X',
+        ' |X| \n-+-+-\nO|O|O\n-+-+-\n |X|X',
+      ],
+    };
+
+    expect(api.botGame('horizontal')).toStrictEqual(expectedResponse);
+  });
 });
