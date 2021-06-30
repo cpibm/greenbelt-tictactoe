acc62c392fbeba9b3d5f3378a300c8bb0d58dc57
Carlos Priego <carlos.priego@ibm.com>
2021-06-29 21:58:46 -0500

    feat(api): finished implementation

--- server.js
+++ server.js
@@ -1,4 +1,42 @@
 const express = require('express');
+const { Replay } = require('./src/replay');
+
+const gameTypes = {
+  horizontal: [
+    { player: 'X', position: 'top-center' },
+    { player: 'O', position: 'mid-left' },
+    { player: 'X', position: 'bottom-center' },
+    { player: 'O', position: 'mid-center' },
+    { player: 'X', position: 'bottom-right' },
+    { player: 'O', position: 'mid-right' },
+  ],
+  diagonal: [
+    { player: 'X', position: 'top-left' },
+    { player: 'O', position: 'top-center' },
+    { player: 'X', position: 'mid-center' },
+    { player: 'O', position: 'mid-left' },
+    { player: 'X', position: 'bottom-right' },
+  ],
+  vertical: [
+    { player: 'X', position: 'top-left' },
+    { player: 'O', position: 'top-center' },
+    { player: 'X', position: 'mid-left' },
+    { player: 'O', position: 'mid-center' },
+    { player: 'X', position: 'bottom-right' },
+    { player: 'O', position: 'bottom-center' },
+  ],
+  draw: [
+    { player: 'X', position: 'bottom-left' },
+    { player: 'O', position: 'top-center' },
+    { player: 'X', position: 'mid-center' },
+    { player: 'O', position: 'top-right' },
+    { player: 'X', position: 'top-left' },
+    { player: 'O', position: 'mid-left' },
+    { player: 'X', position: 'mid-right' },
+    { player: 'O', position: 'bottom-right' },
+    { player: 'X', position: 'bottom-center' },
+  ],
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,9 +44,27 @@ const apiRoot = '/api/tictactoe';
 app.use(express.json());
 
 app.get(`${apiRoot}/newgame`, (req, res) => {
+  const replay = new Replay();
   res.json({
-    appName: process.env.npm_package_name,
-    appVersion: process.env.npm_package_version,
+    winner: replay.winner,
+    board: replay.board.grid,
+    movements: replay.movements,
+    asciiMovements: replay.asciiMovements,
+    player: 'X',
+  });
+});
+
+app.get(`${apiRoot}/botgame/:gameType`, (req, res) => {
+  const movements = gameTypes[req.params.gameType];
+  const replay = new Replay();
+  replay.loadGame(movements);
+  replay.execute();
+
+  res.json({
+    winner: replay.winner,
+    board: replay.board.grid,
+    movements: replay.movements,
+    asciiMovements: replay.asciiMovements,
   });
 });
 
