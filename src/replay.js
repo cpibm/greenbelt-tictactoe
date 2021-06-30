32da6bce053df1c254942014ac62dcec216aeb6c
Carlos Priego <carlos.priego@ibm.com>
2021-06-29 20:20:57 -0500

    test(replay): map movement list

--- src/replay.js
+++ src/replay.js
@@ -1,8 +1,17 @@
 const { Board } = require('./board');
+const { PlayerO } = require('./playerO');
+const { PlayerX } = require('./playerX');
 
 class Replay {
   constructor() {
     this.board = new Board();
+  }
+
+  mapMovementsToPlayers() {
+    return [
+      { player: new PlayerX(), position: 'top-left' },
+      { player: new PlayerO(), position: 'top-center' },
+    ];
   }
 }
 
--- tests/replay.test.js
+++ tests/replay.test.js
@@ -1,6 +1,22 @@
 const { Replay } = require('../src/replay');
+const { PlayerO } = require('../src/playerO');
+const { PlayerX } = require('../src/playerX');
 
 describe('I can replay a saved game', () => {
+  describe('it can load a list of movements', () => {
+    const replay = new Replay();
+
+    test('each movement in list is mapped to the correct player object', () => {
+      const movements = [
+        { player: 'X', position: 'top-left' },
+        { player: 'O', position: 'top-center' },
+      ];
+      const [firstMove, secondMove] = replay.mapMovementsToPlayers(movements);
+      expect(firstMove.player).toBeInstanceOf(PlayerX);
+      expect(secondMove.player).toBeInstanceOf(PlayerO);
+    });
+  });
+
   describe('I can replay a vertical victory saved game', () => {
     const replay = new Replay();
     const { board } = replay;
